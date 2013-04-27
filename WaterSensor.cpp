a935e5ab3d490772554d11699cd3fc9da5385507
supox <supox0@walla.co.il>
2013-04-27 14:57:51 +0300

    Handle water sensor interrupt with scheduler.

--- WaterSensor.cpp
+++ WaterSensor.cpp
@@ -1,5 +1,6 @@
 #include "WaterSensor.h"
 #include "InputsManager.h"
+#include "Scheduler.h"
 
 WaterSensor::WaterSensor(const int id, const int port_index, const double sensor_value, ISensorListener* listener)
 	: Sensor(id, port_index, sensor_value, listener)
@@ -19,13 +20,12 @@ SensorType WaterSensor::GetType()
 	return WATER_READER;
 }
 
-
 void WaterSensor::OnInputChanged(unsigned int input_index, const bool input_value)
 {
 	if( !input_value && this->port_index == input_index ) 
 	{
-		// OnRead would increament reading
-		OnRead(last_reading_value+1);
+		// This is an interrupt routine, update value with the scheduler.
+		Scheduler::AddTask(this);
 	}
 }
 
@@ -33,4 +33,10 @@ bool WaterSensor::ReadSensorFromHardware(double &value)
 {
 	value = this->last_reading_value;
 	return true;
+}
+
+void WaterSensor::DoTask()
+{
+	// Increment current reading:
+		OnRead(last_reading_value+1);
 }
--- WaterSensor.h
+++ WaterSensor.h
@@ -1,17 +1,19 @@
 #pragma once
 #include "Sensor.h"
 #include "IInputListener.h"
+#include "ITask.h"
 
-class WaterSensor : public Sensor, private IInputListener
+class WaterSensor : public Sensor, private IInputListener, private ITask
 {
 public:
 	WaterSensor(const int id, const int port_index, const double sensor_value = 0, ISensorListener* listener = NULL);
 	virtual ~WaterSensor();
 	virtual SensorType GetType();
-	virtual void OnInputChanged(unsigned int input_index, const bool input_value);
 
 
 protected:
 	virtual bool ReadSensorFromHardware(double &value);
+	virtual void OnInputChanged(unsigned int input_index, const bool input_value);
+	virtual void DoTask();
 
 };
